fc09ac3d50becaf97b638486dce142af87ce9ab8
Chao <chaohsulspam@gmail.com>
2022-01-25 00:15:29 +0800

    [change] change timeRemaining units to minute only if hr is 0

--- src/TimeHelper.js
+++ src/TimeHelper.js
@@ -6,7 +6,6 @@ function dayFraction(userTimeZone) {
   return dayFractionAt(userTimeZone, DateTime.now());
 }
 
-
 function dayFractionAt(userTimeZone, dateTime) {
   if(!dateTime) {
     return 0;
@@ -20,14 +19,44 @@ function dayFractionAt(userTimeZone, dateTime) {
   }, {
     zone: userTimeZone
   });
-  return -midnight.diff(dateTime, 'seconds').values.seconds / SECONDS_PER_DAY;
+
+  let fraction = -midnight.diff(dateTime, 'seconds').values.seconds / SECONDS_PER_DAY;
+
+  if(fraction > 1) {
+    while(fraction > 1) {
+      fraction -= 1;
+    }
+  } else if (fraction < 0) {
+    while(fraction < 0) {
+      fraction += 1;
+    }
+  }
+
+  return fraction;
 }
 
 function timeRemaining(dateTime) {
   let diff = dateTime.diff(DateTime.now(), 'seconds').values.seconds;
+
   while(diff < 0) {
-    diff += SECONDS_PER_DAY;  }
-  return Duration.fromMillis(diff * 1000).shiftTo('hours', 'minutes').mapUnits((x, u) => u === 'hours' ? x : Math.ceil(x));
+    diff += SECONDS_PER_DAY;
+  }
+
+  const duration = Duration.fromMillis(diff * 1000).shiftTo('hours', 'minutes').mapUnits(
+    (x, u) => {
+      if(u === 'minutes') {
+        return Math.ceil(x);
+      } else {
+        return x;
+      }
+    }
+  );
+
+  if(duration.hours === 0) {
+    return duration.shiftTo('minutes');
+  }
+
+  return duration;
 }
 
 function timeSimple(userTimeZone, use12Hr, dateTime) {
@@ -35,4 +64,8 @@ function timeSimple(userTimeZone, use12Hr, dateTime) {
   return dateTime.setZone(userTimeZone).toLocaleString(dateFormat);
 }
 
-export { dayFraction, dayFractionAt, timeSimple, timeRemaining };
+function localZoneOffset(){
+  return DateTime.local().zone.formatOffset(Date.now(), 'short');
+}
+
+export { dayFraction, dayFractionAt, timeSimple, timeRemaining, localZoneOffset };
